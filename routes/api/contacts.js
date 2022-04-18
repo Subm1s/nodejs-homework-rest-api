382f910149f6022ae1cd49a2fd38cd5f4a4baa09
Vlad Krizhanivskyi <vlad.krizhanivskyi@gmail.com>
2022-04-18 11:13:41 +0300

    hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,45 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../model/index");
+const {
+  addPostValidation,
+} = require("../../middlewares/validationMiddlewares");
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
+  res.json({ contacts });
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const contact = await getContactById(req.params.contactId);
+  contact.length === 0
+    ? res.status(404).json({ message: "Not found" })
+    : res.json({ contact });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", addPostValidation, async (req, res, next) => {
+  const newContact = await addContact(req.body);
+  res.status(201).json({ newContact });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const contact = await removeContact(req.params.contactId);
+  !contact
+    ? res.status(404).json({ message: "Not found" })
+    : res.json({ message: "contact deleted" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", addPostValidation, async (req, res, next) => {
+  const contact = await updateContact(req.params.contactId, req.body);
+  !contact
+    ? res.status(404).json({ message: "Not found" })
+    : res.json({ contact });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
